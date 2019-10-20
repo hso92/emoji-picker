f9e7b42172fd973e7ea676dcd027ca76d3e43fcf
Kazuya Hashimoto <29920723+hso92@users.noreply.github.com>
2019-10-20 21:29:12 +0900

    update emoji-list

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,12 +3,14 @@ import styled , { ThemeProvider } from "styled-components";
 import GlobalStyle from "../styles/global";
 import theme from "../styles/theme";
 
-
+//===============================
+// @Component
+//===============================
 const Wrapper = styled.div`
   /* max-width: ${props => props.theme.sizes.maxWidth}; */
   position: relative;
   margin: 0 auto;
-  padding: 0 ${props => props.theme.sideSpace.large};
+  padding: 5em ${props => props.theme.sideSpace.large};
   @media screen and (max-width: ${props => props.theme.responsive.large}) {
     max-width: 760px;
   }
--- src/components/list.tsx
+++ src/components/list.tsx
@@ -1,30 +1,88 @@
-import React from 'react'
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 import { Store } from "../store";
 import { TYPE_EMOJI } from "../types/types";
+import { useCopyToClipboard } from "react-use";
 
-const View : React.FC = (props:any) => {
+//===============================
+// @Component
+//===============================
+const View: React.FC = (props: any) => {
+  const emojiRef = React.useRef<any>(null);
+  const [text, copyToClipboard] = useCopyToClipboard();
+  const [toggle, setToggle] = React.useState(false);
+  React.useEffect(() => {}, [emojiRef]);
+
   const { state } = React.useContext(Store);
   const emoji = state.data;
   return (
     <div className={props.className}>
-      <h1 className="title">EMOJI LIST</h1>
       <ul className="emoji-list">
         {emoji.map((item: TYPE_EMOJI) => (
-          <li key={item.emoji}>{item.emoji}</li>
+          <li key={item.emoji} className="emoji">
+            <button
+              onClick={() => {
+                copyToClipboard(item.emoji);
+                setToggle(true);
+                setTimeout(() => setToggle(false), 2000);
+              }}
+              ref={emojiRef}
+            >
+              {item.emoji}
+            </button>
+          </li>
         ))}
       </ul>
+      <div className={`alert ${toggle ? "is-in" : ""}`}>
+        <p>copied!</p>
+      </div>
     </div>
   );
-}
+};
 
+//===============================
+// @Styled
+//===============================
 export default styled(View)`
-  .title {
-    font-weight: 700;
-    font-size:2.4rem;
+  > .emoji-list {
+    display: flex;
+    align-items: center;
   }
-  .emoji-list {
-    display:flex;
-    align-items:center;
+  .emoji {
+    font-size: 2.4rem;
+    line-height: 1;
+    &:hover {
+      transform: scale(1.1);
+    }
+    > button {
+      cursor: pointer;
+      line-height: inherit;
+    }
   }
-`
+  .alert {
+    position: fixed;
+    left: 0;
+    right: 0;
+    bottom: 5em;
+    margin: auto;
+    border-radius: 25px;
+    width: 30%;
+    text-align: center;
+    background: ${props => props.theme.colors.blue5};
+    box-shadow: 0 0 10px ${props => props.theme.colors.gray};
+    transition: .2s ease-in-out;
+    visibility: hidden;
+    opacity: 0;
+    padding:.5em 0;
+    letter-spacing:.05em;
+    &.is-in {
+      opacity: 1;
+      visibility: visible;
+    }
+    > p {
+      font-weight: 700;
+      font-size: 2rem;
+      color: #fff;
+    }
+  }
+`;
